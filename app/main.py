c210ca668d3f2bcd3d947d291913c484f83bb395
Cesar <cesarmsoto201@gmail.com>
2026-08-27 15:43:55 -0700

    fix some issue

--- app/main.py
+++ app/main.py
@@ -3,11 +3,12 @@ from app.models import todo
 app = FastAPI()
 
 todos = [
-    {"id":1, "title":"Monday", "description":"clean the room", "completed":False},
-    {"id":3, "title":"Tuesday", "description":"mop the house", "completed":False},
-    {"id":4, "title":"Sunday", "description":"Go to the gym", "completed":False},
-    {"id":7, "title":"Friday", "description":"Go for a 20min run", "completed":False},
+    todo(id=1, title="Monday", description="clean the room", completed=True),
+    todo(id=3, title="Tuesday", description="mop the house", completed=False),
+    todo(id=4, title="Sunday", description="go to the gym", completed=True),
+    todo(id=7, title="Friday", description="Go for a 20min run", completed=False)
 ]
+    
 
 @app.get("/")
 def greet():
