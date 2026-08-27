8ee99b01bc65f83df52506e4f470022e5f61dea4
Cesar <cesarmsoto201@gmail.com>
2026-08-27 15:25:34 -0700

    added models file

--- app/main.py
+++ app/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
-
+from models import todo
 app = FastAPI()
 
-@App
+todos = [
+    {"id":1, "title":"Monday", "description":"clean the room", "completed":False},
+    {"id":3, "title":"Tuesday", "description":"mop the house", "completed":False},
+    {"id":4, "title":"Sunday", "description":"Go to the gym", "completed":False},
+    {"id":7, "title":"Friday", "description":"Go for a 20min run", "completed":False},
+]
+
+@app.get("/")
 def greet():
-    print("Hello to the user")
+    return "Hello to the user"
+
+@app.get("/todos")
+def get_all_todos():
+    return todos
